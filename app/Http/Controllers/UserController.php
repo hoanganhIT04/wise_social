b953e2fcf78bba4aceeea47b36aaf9e445e4b0f6
hoanganh_it_04 <anhemhucau@gmail.com>
2024-12-21 17:16:38 +0700

    select data

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\models\User;
+use App\Helpers\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+    private $apiResponse;
+
+    public function __construct() {
+        $this->apiResponse = new ApiResponse();
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @return string JSON
+     */
+    public function show(Request $request)
+    {
+        $userId = Auth::id();
+        if (!$userId) {
+            return $this->apiResponse->UnAuthorization();
+        }
+        $user = User::with('follower', 'follows')
+            ->select(
+                'id', 'name', 'email',
+                'avatar', 'overview'
+            )->where('id', $userId)->first();
+        $user->followers = count($user->follower);
+        $user->following = count($user->follows);
+        unset($user->follower, $user->follows);
+        return $this->apiResponse->success($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -50,4 +51,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Realationship function get follower
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function follower()
+    {
+        return $this->HasMany('\App\Models\Follow', 'user_id', 'id')
+            ->where('user_id', '<>', Auth::user()->id)
+            ->where('follow_id', Auth::user()->id);
+    }
+
+    /**
+     * Realationship function get follower
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function follows()
+    {
+        return $this->HasMany('\App\Models\Follow', 'user_id', 'id')
+            ->where('user_id', Auth::user()->id);
+    }
 }
+
